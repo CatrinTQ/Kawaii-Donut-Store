b5de7bcf50fb506dd78fdfa0a7acf07a30d6c711
Catrin Tornqvist <catrin.tq@gmail.com>
2024-11-27 16:44:06 +0100

    extracted productlist to module

---
+++ products.mjs
@@ -0,0 +1,60 @@
+const products = [
+    {
+      id: 0,
+      name: 'Tiger',
+      price: 35,
+      rating: 4,
+      amount: 0,
+      category: 'sweet',
+      img: {
+        url: './assets/photos/product-image-1.png',
+        width: 50,
+        heigt: 50,
+        alt: ''
+      },
+    },
+    {
+      id: 1,
+      name: 'Snölejon',
+      price: 30,
+      rating: 5,
+      amount: 0,
+      category: 'animal',
+      img: {
+        url: './assets/photos/product-image-2.png',
+        width: 50,
+        heigt: 50,
+        alt: ''
+      },
+    },
+    {
+      id: 2,
+      name: 'Blå tiger',
+      price: 250,
+      rating: 2.5,
+      amount: 0,
+      category: 'animal',
+      img: {
+        url: './assets/photos/product-image-3.png',
+        width: 50,
+        heigt: 50,
+        alt: ''
+      },
+    },
+    {
+      id: 3,
+      name: 'Kanin',
+      price: 40,
+      rating: 1.5,
+      amount: 0,
+      category: 'animal',
+      img: {
+        url: './assets/photos/product-image-4.png',
+        width: 50,
+        heigt: 50,
+        alt: ''
+      },
+    },
+  ];
+
+  export default products;
